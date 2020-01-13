8a570258544b1da67cb5bc4f663942805303bbc4
kousuke.furuta <kaientai.6260@gmail.com>
2020-01-13 16:12:39 +0900

    Add getAlltasks

--- src/controllers2/IndexServlet2.java
+++ src/controllers2/IndexServlet2.java
@@ -1,19 +1,24 @@
 package controllers2;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models2.Task;
+import utils2.DBUtil2;
 /**
  * Servlet implementation class IndexServlet2
  */
 @WebServlet("/index")
 public class IndexServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -25,9 +30,13 @@ public class IndexServlet2 extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil2.createEntityManager();
 
+        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
+                                   .getResultList();
+        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+
+        em.close();
+    }
 }
